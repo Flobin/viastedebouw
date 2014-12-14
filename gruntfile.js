8eed5cfb79e124ac1c66e3560b0cbb0050a29149
Robin Berghuijs <robin@robinberghuijs.com>
2014-12-14 18:16:51 +0100

    fix font rendering in chrome on windows
    see https://github.com/omeka/theme-berlin/issues/10

--- gruntfile.js
+++ gruntfile.js
@@ -1,15 +1,12 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         concat: {
-            // 2. Configuration for concatinating files goes here.
             dist: {
                 src: [
-                    'js/*.js', // All JS in the libs folder
-                    'js/global.js'  // This specific file
+                    'js/*.js'
                 ],
                 dest: 'js/build/production.js',
             }
@@ -38,7 +35,7 @@ module.exports = function(grunt) {
                 files: {
                     'style.css': 'sass/style.scss'
                 }
-            } 
+            }
         },
         watch: {
             options: {
@@ -69,14 +66,12 @@ module.exports = function(grunt) {
 
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'sass']);
 
 };
