09c324550ce38f33e1ea516f4bde167d14af4f7b
Robin Berghuijs <robin@robinberghuijs.com>
2016-03-21 14:41:23 +0100

    update graduate passport

--- content-graduate_passport.php
+++ content-graduate_passport.php
@@ -10,15 +10,29 @@
 		<div class="graduate-thumbnail"><?php
 			if ( has_post_thumbnail() ) {
 				the_post_thumbnail(array(300,300));
-			} 
+			}
 		?></div>
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<section class="entry-content">
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'viastedebouw' ), 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
+	</section><!-- .entry-content -->
+    <section class="entry-content">
+        <hr>
+        <table class="graduate-passport-table">
+            <tr>
+                <td>Afstudeeronderwerp</td>
+                <td><?php the_field('afstudeeronderwerp'); ?></td>
+            </tr>
+            <tr>
+                <td>Link</td>
+                <td><a href="<?php the_field('link'); ?>"><?php the_field('link_titel'); ?></a></td>
+            </tr>
+        </table>
+    </section>
+
 
 	<footer class="entry-meta">
 		<?php
